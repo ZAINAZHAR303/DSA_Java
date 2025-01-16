50ef1935dc4548c712d5a407dd15684bc13e63df
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-01-16 10:59:54 +0500

    Even Odd number in java

---
+++ java_learning/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+//        int rollno = 64;
+//        char letter = 'A';
+//        float marks = 98.67f;
+//        double large = 876857687.45;
+//        long largeinteger = 765897977765786L;
+//        boolean check = true;
+
+    }
+}
