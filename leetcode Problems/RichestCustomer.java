86027570c4e601910a5697861a5a86aeb15a3810
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-01-17 22:05:01 +0500

    solve RichestCustomer Problem in leetcode

---
+++ leetcode Problems/RichestCustomer.java
@@ -0,0 +1,24 @@
+package com.kunal;
+
+public class RichestCustomer {
+    public int maximumWealth(int[][] accounts) {
+        int wealth = Integer.MIN_VALUE;
+
+        for(int[] ints :accounts){
+
+            // int[] ints = accounts[i];
+            int sum =0;
+            for(int anint : ints){
+                sum += anint;
+                // sum = sum+ints[j];
+
+
+            }
+            if(sum>wealth){
+                wealth= sum;
+
+            }
+        }
+        return wealth;
+    }
+}
