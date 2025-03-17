fcc8edca2888429257ffa33e9bde4d671afc5396
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-03-17 21:08:54 +0500

    use hashset to solve 217 for o(n) instead of o(n2)

--- leetcode Problems/leetcodw/src/Contains_duplicate.java
+++ leetcode Problems/leetcodw/src/Contains_duplicate.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class Contains_duplicate {
     public static void main(String[] args) {
      int [] nums = {1,2,3,1};
@@ -5,16 +7,27 @@ public class Contains_duplicate {
     }
 
 
-        static boolean containsDuplicate(int[] nums) {
-            for (int i = 0; i <nums.length; i++){
-                for(int j=0 ; j<nums.length ; j++){
-                    if(nums[i]==nums[j] && i!=j){
-                        return true;
-                    }
-                }
-            }
+//        static boolean containsDuplicate(int[] nums) {
+//            for (int i = 0; i <nums.length; i++){
+//                for(int j=0 ; j<nums.length ; j++){
+//                    if(nums[i]==nums[j] && i!=j){
+//                        return true;
+//                    }
+//                }
+//            }
+//
+//            return false;
+//        }
 
-            return false;
+    static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
         }
+        return false;
+    }
 
 }
