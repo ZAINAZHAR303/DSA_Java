1b50cbf468a0a9002d9f283de363ca6b1088b265
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-06-02 11:48:24 +0500

    numbers pattern in java

--- java_learning/patterns/Main.java
+++ java_learning/patterns/Main.java
@@ -1,7 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        Pattern5(5);
+        Pattern30(5);
     }
+
+    static void Pattern30(int n){
+        for(int row = 1;row<=n;row++){
+            for (int space = 0 ; space< n-row; space++){
+                System.out.print("  ");
+            }
+            for (int col=row; col>=1;col--){
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <=row;col++){
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+    static void Pattern28(int n){
+        for(int row=1;row<=2*n;row++){
+            int TotalcolsinRow = row>n? 2*n-row : row;
+//            int spaces = row>n ? row-n : n-row;
+            int spaces = n-TotalcolsinRow;
+            for (int space =1;space<=spaces;space++){
+                System.out.print(" ");
+            }
+            for(int col = 1; col<=TotalcolsinRow;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+
     static void Pattern5(int n){
         for(int row = 1; row<=2*n;row++){
             int TotalColinRow = row> n ? 2*n-row : row;
