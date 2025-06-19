846b2531e72b449d214d5a2d4c8ef450fa94e70b
ZAINAZHAR303 <zainazhar303@gmail.com>
2025-06-19 21:42:55 +0500

    full pyramid pattern with stars

--- java_learning/patterns/StarPatternRevise.java
+++ java_learning/patterns/StarPatternRevise.java
@@ -1,6 +1,6 @@
 public class StarPatternRevise {
     public static void main(String[] args) {
-        StarPattern(5);
+        spacePattern(5);
     }
     static void StarPattern(int n){
         for(int i = 1; i<=n;i++){
@@ -10,5 +10,18 @@ public class StarPatternRevise {
             System.out.println();
         }
 
+    }
+    static void spacePattern(int n){
+        for (int i = 1; i<=n;i++){
+            for (int l=1;l<= n-i;l++){
+                System.out.print(" ");
+            }
+            for (int j= 1;j<=i;j++){
+
+                System.out.print(" * ");
+
+            }
+            System.out.println();
+        }
     }
 }
