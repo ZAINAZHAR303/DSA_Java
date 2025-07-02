70957ac40a4d7575db45dbf00456c0f18529e0d5
ZAINAZHAR303 <zainazhar303@gmail.com>
2025-07-02 23:07:14 +0500

    initial code for find fabonacci number

--- java_learning/recursion/Fabonacci.java
+++ java_learning/recursion/Fabonacci.java
@@ -2,6 +2,16 @@ package recursion;
 
 public class Fabonacci {
     public static void main(String[] args) {
+        int x= 5;
+    FindFabonaci(x);
+    }
+
+    static int FindFabonaci(int n){
+        if (n==1 && n==0){
+            return n;
+        }
+
+
 
     }
 }
