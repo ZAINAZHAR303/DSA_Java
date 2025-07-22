0acace1e986e9610c48ffbae2d8198a589ecf020
ZAINAZHAR303 <zainazhar303@gmail.com>
2025-07-22 22:20:27 +0500

    understand fabonacci with recursion

---
+++ java_learning/recursion/UnderstandFibonacci.java
@@ -0,0 +1,22 @@
+public class UnderstandFibonacci {
+
+    public static void main(String[] args) {
+        int n = 7;  // Change this number to find a different Fibonacci number
+        System.out.println("Fibonacci number at position " + n + " is: " + fibonacci(n));
+
+
+
+    }
+    public static int fibonacci(int n) {
+        // Base cases: 0th = 0, 1st = 1
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        // Recursive call: fib(n) = fib(n-1) + fib(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
